66c788d661a211078c421a9fed385c0d302dd84f
Robert Bethencourt <robert.bethencourt@gmail.com>
2017-04-05 12:15:48 -0400

    postcss-loader added

--- src/Components/Home/HomeContainer.js
+++ src/Components/Home/HomeContainer.js
@@ -25,7 +25,7 @@ class HomeContainer extends React.Component {
     axios.get('/api/test')
       .then(response => {
         this.setState({
-          isFetching: false,
+          isFetching: true,
           users: response.data
         })
       })
