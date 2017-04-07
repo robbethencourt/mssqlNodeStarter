52c7b80789edc3d4eb2cf7d7e87bd7a2990056ab
Robert Bethencourt <robert.bethencourt@gmail.com>
2017-04-07 15:14:18 -0400

    postcss added

--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -3,19 +3,18 @@ const config = require('../../config/mssqlConnection')
 
 module.exports = function (app) {
   app.get('/api/test', function (req, res) {
-    var conn = new sql.Connection(config)
-    var request = new sql.Request(conn)
-
-    conn.connect(function (err) {
-      if (err) console.log(err)
-      request.query('select * from NodeTestTable', function (err, recordset) {
-        if (err) {
-          console.log(err)
-        } else {
-          res.send(recordset)
-        }
-        conn.close()
-      }) // end request.query()
-    }) // end conn.connect()
+    sql.connect(config)
+      .then(() => {
+        return sql.query`select * from NodeTestTable`
+      })
+      .then(result => {
+        res.send(result)
+      })
+      .catch(err => {
+        res.send(`error from database: ${err}`)
+      }) // end sql.connect()
+    sql.on('error', err => {
+      res.send(`error from database: ${err}`)
+    }) // end sql.on()
   }) // end app.get('/api/test')
 }
